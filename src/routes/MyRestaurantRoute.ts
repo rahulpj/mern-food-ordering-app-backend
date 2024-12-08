9986a6ecbc93b3dc53938aac6adeee1068b194a3
Rahul Panjwani <rahulpanjwani2003@gmail.com>
2024-12-08 21:57:00 +0530

    adding update restaurant feature

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -27,4 +27,13 @@ router.post(
   MyRestaurantController.createMyRestaurant
 );
 
+router.put(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant
+);
+
 export default router;
